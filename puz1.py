847e66f9fc0e920de5d42f399ff14d5ec425aa94
Peter Tempfli <tempflip@gmail.com>
2019-02-05 16:26:31 +0100

    w

--- puz1.py
+++ puz1.py
@@ -10,6 +10,12 @@ def korrekt(w=3):
 	x[w-1][w-1] = 0
 	return x
 
+def print_nice(puzz):
+	for i in range(len(puzz)):
+		print(puzz[i])
+	print('.............................')
+
+
 def where_is_element(puzz, el=0):
 	for i in range(len(puzz)):
 		if el in puzz[i]:
@@ -18,12 +24,21 @@ def where_is_element(puzz, el=0):
 					return (i, j )
 
 
-def move(puzz, where, which_el=0):
+def move(puzz, dir, which_el=0):
 	
-	if (where == 0): # up
-		puss
+	if (dir == 0): # up
+		pos = where_is_element(puzz, which_el) # the 0
+		buff = puzz[pos[0]-1][pos[1]]
+		puzz[pos[0]-1][pos[1]] = puzz[pos[0]][pos[1]]
+		puzz[pos[0]][pos[1]] = buff
 
+
+
+	return puzz
 	
 
 puzz = korrekt(3)
-print(where_is_element(puzz))
+print_nice(puzz)
+
+puzz = move(puzz, 0, 3)
+print_nice(puzz)
