e65d6aac1ecdcea167f5999c36df60cf68133e7f
Peter Tempfli <tempflip@gmail.com>
2019-02-19 16:31:09 +0100

    w

--- search-DFS-heur1.py
+++ search-DFS-heur1.py
@@ -16,6 +16,13 @@ def heur_manhattan(goal, b):
 		score += manhattan_cost(b_pos, goal_pos, SIZE)
 	return score
 
+def route_to(start, goal, parent_map): # calculates the route. Start is the END, so it has to have a parent in the map
+	path = [start]
+	while path[-1] != goal:
+		st = path[-1]
+		par = parent_map[st]
+		path.append(par)
+	return path
 
 goal_pos = korrekt(3)
 # states = shuffle(goal_pos, steps=60)
@@ -28,14 +35,21 @@ SIZE = 3
 # start_pos = (1, 2, 4, 7, 5, 6, 3, 0, 10, 13, 11, 8, 9, 14, 15, 12)
 ## 
 # start_pos = (0, 1, 5, 4, 3, 2, 7, 8, 6) #10
-start_pos = (2, 7, 3, 1, 0, 6, 5, 4, 8) #20
+# start_pos = (2, 7, 3, 1, 0, 6, 5, 4, 8) #20
 # start_pos = (1, 2, 3, 5, 0, 8, 4, 6, 7) #50
+
+### 1000
+# start_pos = (1, 4, 7, 3, 5, 8, 0, 2, 6)
+# start_pos = (6, 2, 7, 1, 4, 5, 0, 3, 8)
+# start_pos = (2, 6, 5, 7, 8, 1, 3, 4, 0)
+start_pos = (2, 7, 4, 8, 6, 5, 3, 1, 0)
 
 print_nice(start_pos)
 
 to_go = [start_pos]
 seen = []
 parent_map = {}
+score_map = {}
 
 start_time = time.time()
 i = 0
@@ -45,45 +59,52 @@ my_heur = heur_manhattan
 
 while True:
 	state = to_go.pop()
-	print ('###', state, my_heur(goal_pos, state))
+	# print ('###', state, my_heur(goal_pos, state))
 
 	seen.append(state)
  
+ 	#### this is the goal
 	if (state == goal_pos):
 		print('WOOOOOO HOOOO')
 		break
 
+
+	#### generating next states
 	next_states_with_weights = sorted([(state, my_heur(goal_pos, state)) # st, weight
 							for state in next_states(state) # the loop
 						], key = lambda x: x[1], reverse = True) # sort by weights
+	
 	next_sts = [state for state, weight in next_states_with_weights] # get rid of weights
+
+	#### saving the scores for A*
+	for st, weight in next_states_with_weights:
+		score_map[st] = weight + len(route_to(state, start_pos, parent_map))
+		# print (score_map)
 
 
 	# next_sts = next_states(state) -- just for testing -- DFS without heuristics
-	# print (state)
-	# print (next_states_with_weights)
 
 	for next_state in next_sts:
 		if next_state in seen : continue
 		parent_map[next_state] = state # saving the parent, so we can get the route when found the right way
 		to_go.append(next_state)
 
+
+	# re-order the queue
+	to_go = sorted(to_go, key = lambda x:score_map[x], reverse = True)
+
+ 	# some output
 	i+= 1
 	if i % 100 == 0 : print (i)
 
 total_time = (time.time() - start_time) * 1000 * 1000
 
 ### get the route
-path = [goal_pos]
-while path[-1] != start_pos:
-	st = path[-1]
-	par = parent_map[st]
-	# print(a_to_b(st, par))
-	# print (path[-1])
-	path.append(par)
+# print (parent_map)
+path = route_to(goal_pos, start_pos, parent_map)
 
-
-# print(parent_map)
+for st in path:
+	print (st)
 
 print ('Iterations: ', i)
 print ('Total time: ', total_time)
